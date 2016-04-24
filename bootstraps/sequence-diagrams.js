cf8d61d27ece152ea640a6d96dd042dcbb3397f8
Antoine@roz-isit <trouve@soc.ait.kyushu-u.ac.jp>
2016-04-25 06:59:39 +0900

    Adds static verson of jQuery et cie

--- bootstraps/sequence-diagrams.js
+++ bootstraps/sequence-diagrams.js
@@ -11,5 +11,9 @@ hadooc.sequenceDiagrams = { }
 hadooc.sequenceDiagrams.init = function(parent) {
   if(!parent) { parent = $("body") }
   
-  parent.find(".source-code.sequenceDiagrams").each(function(id, e) { Diagram.parse(e.value).drawSVG($(e).next().attr("id"), { theme:"simple" } ) } )
+  parent.find(".source-code.sequenceDiagrams").each(function(id, e) {
+    var targetId = $(e).next().attr("id")
+    console.log("Print a sequence diagram to #" + targetId)
+    Diagram.parse(e.value).drawSVG(targetId, { theme:"simple" } )
+  })
 }
---
+++ static_dependencies/README.md
@@ -0,0 +1,1 @@
+# Static dependencies for hadooc
